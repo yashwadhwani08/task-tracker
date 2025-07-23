2b35ffc8aff1c7001394610d917dafa33b9fc3f3
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-23 19:23:20 +0530

    packaging different components into specific modules and leveraging the use of shared-modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
+// import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
 // import { DatePipe } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, TaskComponent, NewTaskComponent],
-//   imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    // CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
+  //   imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+//   imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
   exports: [],
   bootstrap: [AppComponent],
 })
